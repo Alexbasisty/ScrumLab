c7562eb205573541099965d5bd0457fdc0dfeabb
Alex Basystyi <alexbasisty@gmail.com>
2020-01-11 12:25:51 +0100

    event on switching add-page

--- development/js/app.js
+++ development/js/app.js
@@ -27,3 +27,34 @@ if (formEl !== null) {
 
 loadPage();
 
+
+
+//    adding recipe
+
+const addRecipeButton = document.querySelector('.top-bar .fa-plus-square.add-recipe');
+const listRecipesSection = document.querySelector('section.list-of-recipes');
+const addRecipeSection = document.querySelector('section.add-receipe-form');
+const saveCloseButton = document.querySelector('button.save-close');
+const addInstructionsButton = document.querySelector('.recipe-instruction .add-recipe');
+const addDescriptionButton = document.querySelector('.recipe-description .add-recipe');
+
+
+addRecipeButton.addEventListener('click', function (event) {
+        listRecipesSection.style.display = 'none';
+        addRecipeSection.classList.toggle('hidden');
+});
+
+saveCloseButton.addEventListener('click', function (event) {
+    listRecipesSection.style.display = 'inherit';
+    addRecipeSection.classList.toggle('hidden');
+});
+
+addInstructionsButton.addEventListener('click', function () {
+    const instructionFieldValue = document.querySelector('#new-receipe-instruction').value;
+    console.log(instructionFieldValue);
+});
+
+addDescriptionButton.addEventListener('click', function (event) {
+    const descriptionFieldValue = document.querySelector('#new-receipe-ingredient').value;
+    console.log(descriptionFieldValue);
+});
