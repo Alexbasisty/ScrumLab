a34e367fcb4a031fce43f5ebe1d13ab3209ddd63
Alex Basystyi <alexbasisty@gmail.com>
2020-01-13 12:57:48 +0100

    load list of recipes from LS

--- development/js/app.js
+++ development/js/app.js
@@ -1,4 +1,3 @@
-
 const formEl = document.querySelector('.name-insert');
 const userDefault = document.querySelector('.user h2');
 const greetingSection = document.querySelector('.greeting-section');
@@ -40,6 +39,15 @@ const addDescriptionButton = document.querySelector('.recipe-description .add-re
 const instructionListEl = document.querySelector('.instruction-list');
 const descriptionList = document.querySelector('.description-list ul');
 let counter = 0;
+let allRecipes = [];
+
+    if (localStorage.getItem("recipe") !== "null") {
+        JSON.parse(localStorage.recipe).forEach(function (element) {
+            allRecipes.push(element);
+        })
+    }
+
+
 
 function correctLiNumber() {
     const allLiCounters = document.querySelectorAll('#recipeCounter');
@@ -142,35 +150,38 @@ document.querySelector('.recipe-instruction').addEventListener('click', function
 // ***********************************
 
 saveCloseButton.addEventListener('click',function () {
-    class Recipe{
-        constructor(name, description, instruction, ingredients){
-            this.name = name;
-            this.description = description;
-            this.instruction = instruction;
-            this.ingredients = ingredients;
-        }
+    function Recipe(id, title, description) {
+        this.id = id; // id przepisu
+        this.title = title; // nazwa przepisu
+        this.description = description; // opis przepisu
+        this.ingredients = []; // składniki przepisu
+        this.instructions = []; // instrukcje przepisu
     }
+
     const nameRecipeValue = document.querySelector('#new-receipe-name').value;
     const descriptionFieldValue = document.querySelector('#new-receipe-description').value;
     const allRecipesDivEl = document.querySelector('.all-recipes');
-    const recipe = new Recipe(nameRecipeValue, descriptionFieldValue);
+
+
 
 
  if (nameRecipeValue.length > 0 && descriptionFieldValue.length > 0) {
-     let recipeKey = 'recipe' + counter;
-     console.log(recipeKey);
-     console.log(counter);
+     // let recipeKey = 'recipe' + counter;
+     // console.log(recipeKey);
+     // console.log(counter);
 
-     localStorage.setItem(recipeKey, JSON.stringify(recipe));
+     const recipeKey = new Recipe(allRecipes.length + 1, nameRecipeValue, descriptionFieldValue);
+     allRecipes.push(recipeKey);
+
+     console.log(recipeKey);
+     localStorage.setItem("recipe", JSON.stringify(allRecipes));
      const newUl = document.createElement('ul');
      newUl.classList.add('recipe');
 
-     const recipeFromLS = JSON.parse(localStorage['recipe' + counter]);
-
-     const newUlElments = `                             
+     newUl.innerHTML = `
                     <li id="recipeCounter">1</li>
-                    <li>${recipeFromLS.name}</li>
-                    <li>${recipeFromLS.description}</li>
+                    <li>${recipeKey.title}</li>
+                    <li>${recipeKey.description}</li>
                     <li>
                         <i class="far fa-edit edit-recipe"></i>
                         <i class="far fa-trash-alt delete-recipe"></i>
@@ -180,10 +191,10 @@ saveCloseButton.addEventListener('click',function () {
                         <i class="fas fa-print"></i>
                     </li>
          `;
-     newUl.innerHTML = newUlElments;
+
      allRecipesDivEl.appendChild(newUl);
      counter++;
-     document.querySelector('#new-receipe-name').value = ""
+     document.querySelector('#new-receipe-name').value = "";
      document.querySelector('#new-receipe-description').value = ""
  }
     // correct recipe numbers in list
@@ -191,4 +202,93 @@ saveCloseButton.addEventListener('click',function () {
     document.querySelector('.list-of-recipes').addEventListener('click', correctLiNumber);
     saveCloseButton.addEventListener('click', correctLiNumber);
 });
+function loadRecipesList() {
+    if (localStorage.getItem("recipe") !== "null") {
+        const recipeObjects = JSON.parse(localStorage.recipe);
+        recipeObjects.forEach(function (element) {
+            const newUl = document.createElement('ul');
+            newUl.classList.add('recipe');
+            const allRecipesDivEl = document.querySelector('.all-recipes');
+
+            newUl.innerHTML = `
+                    <li id="recipeCounter">1</li>
+                    <li>${element.title}</li>
+                    <li>${element.description}</li>
+                    <li>
+                        <i class="far fa-edit edit-recipe"></i>
+                        <i class="far fa-trash-alt delete-recipe"></i>
+                        <br>
+                        <i class="far fa-clone"></i>
+                        <i class="far fa-file-pdf"></i>
+                        <i class="fas fa-print"></i>
+                    </li>
+         `;
+            allRecipesDivEl.appendChild(newUl);
+        })
+        }
+}
+loadRecipesList();
+correctLiNumber();
+
+/*
+ **********************************************************************
+ * ********************************************************************
+ */
+
+function Recipe(id, title, description) {
+    this.id = id; // id przepisu
+    this.title = title; // nazwa przepisu
+    this.description = description; // opis przepisu
+    this.ingredients = []; // składniki przepisu
+    this.instructions = []; // instrukcje przepisu
+}
+/*
+ Metoda `.showInfo()`
+ wyświetlająca w konsoli wszystkie informacje o przepisie */
+Recipe.prototype.showInfo = function() {
+    console.warn(this.id, this.title); // wyświetl id oraz tytuł
+    console.warn(this.description); // wyświetl opis
+    this.ingredients.map(function(elem, i) {
+        console.warn(i, elem); // wyświetl każdy element
+    })
+    this.instructions.map(function(elem, i) {
+        console.warn(i, elem); // wyświetl każdy element
+    })
+}
+
+/*
+Metoda `.saveToLocalStorage()`
+zapisująca do localStorage informacje o przepisie */
+Recipe.prototype.saveToLocalStorage = function() {
+    /* if(){
+        // uzupełnij
+      }else{
+        // uzupełnij
+      } */
+}
+
+// przygotowanie globalnej zmiennej przechowującej wszystkie przepisy
+var allRecipies = [];
+
+// utworzenie kilku przykładowych przepisów
+var newRecipe1 = new Recipe(allRecipies.length + 1, "Jajecznica na boczku", "Taką jajecznicę lubie najbardziej ;p ");
+allRecipies.push(newRecipe1); // dodanie przepisu do globalnej tablicy
+var newRecipe2 = new Recipe(allRecipies.length + 1, "Fasolka po bretońsku", "Taka fasolka że kołdrę podnosi!");
+allRecipies.push(newRecipe2);
+var newRecipe3 = new Recipe(allRecipies.length + 1, "Sałatka grecka", "Oryginalna sałatka grecka z pomidora, ogórka, czerwonej cebuli i czarnych oliwek, z oliwą i oregano. ");
+allRecipies.push(newRecipe3);
+
+// dodawanie składników do przepisu (newRecipe1, allRecipies[0])
+newRecipe1.ingredients.push("3 jajka");
+newRecipe1.ingredients.push("mała cebula");
+newRecipe1.ingredients.push("szczypiorek");
+newRecipe1.ingredients.push("5 plasterków boczku");
+
+newRecipe1.instructions.push("Rozpuść masło na patelni i podgrzej.");
+newRecipe1.instructions.push("Dodaj boczek.");
+newRecipe1.instructions.push("Na rozgrzaną patelnię wbij jajaka i mieszaj doprawiając.");
+newRecipe1.instructions.push("Podawaj z grzankami. Smacznego!");
+
+console.clear();
+allRecipies[0].showInfo(); // wyświetla pierwszy przepis w konsoli
 
